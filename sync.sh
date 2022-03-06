a6a14838ae0761d9f785aa40eed7b914f347ec09
Mobiacikaka <stable.justin@gmail.com>
2022-03-06 16:42:48 +0800

    add gotop

--- sync.sh
+++ sync.sh
@@ -4,5 +4,12 @@ set -e
 
 backup_path=$(pwd)
 
-rsync -av $HOME/.config/i3/ "$backup_path/.config/i3"
-rsync -av $HOME/.config/nvim/ "$backup_path/.config/nvim"
+sync()
+{
+	echo rsync -a "$HOME/$1/$2" "$backup_path/$1"
+	rsync -a "$HOME/$1/$2" "$backup_path/$1"
+}
+
+sync .config i3
+sync .config nvim
+sync .config gotop
